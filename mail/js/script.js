a9d455d5d7496cecff62fdfa93952844a08e03ea
Danilo <danilo.campagnolo.93@gmail.com>
2020-01-07 15:27:00 +0100

    mail complete

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,5 +1,22 @@
 var mailUtente;
 var database;
+var trovato = false;
 
 mailUtente = prompt("Inserisci la tua mail");
 console.log (mailUtente);
+
+database = ["esempio1@mail", "esempio2@mail", "esempio3@mail", "esempio4@mail", "danilo@mail"];
+
+for (var i = 0; i < database.length; i++) {
+  if (mailUtente == database[i]) {
+    var trovato = true;
+  }
+}
+console.log(trovato);
+
+if (trovato == true) {
+  console.log("mail trovata");
+}
+else {
+  console.log("mail non trovata");
+}
