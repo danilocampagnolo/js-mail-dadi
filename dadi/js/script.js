338484f604a6bc592a37a9ec723ff62b1f7c88ca
Danilo <danilo.campagnolo.93@gmail.com>
2020-01-07 15:06:40 +0100

    dadi complete

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,2 +1,18 @@
-var numGiocatore1
-var numGiocatore2
+var dadoGiocatore1
+var dadoGiocatore2
+
+dadoGiocatore1 = Math.floor(Math.random() * 6) + 1;
+console.log(dadoGiocatore1);
+
+dadoGiocatore2 = Math.floor(Math.random() * 6) + 1;
+console.log(dadoGiocatore2);
+
+if (dadoGiocatore1 > dadoGiocatore2) {
+  console.log("il giocatore 1 ha vinto")
+}
+else if (dadoGiocatore1 == dadoGiocatore2) {
+  console.log("pari")
+}
+else {
+  console.log("il giocatore 2 ha vinto");
+}
